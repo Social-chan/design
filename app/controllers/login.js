9fd626f2f4a0eef9cbd562f2f8829bcba1dd80e0
d8vjork <d8vjork@outlook.com>
2018-01-09 00:41:08 +0100

    Netlify small changes

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,5 +1,30 @@
 import Controller from '@ember/controller';
+import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency';
 
 export default Controller.extend({
+  session: service(),
 
+  authenticate: task(function* () {
+    yield this.get('session').authenticate(
+      'authenticator:oauth2',
+      identification, password,
+      ['*'],
+      {
+        'Content-Type': 'application/json',
+      }
+    ).catch((reason) => {
+      this.set('errorMessage', reason.error);
+    }).then((response) => {
+      this.transitionTo('feed');
+    });
+
+
+  }).drop(),
+
+  actions: {
+    authenticate() {
+      this.get('authenticate').perform();
+    }
+  }
 });
