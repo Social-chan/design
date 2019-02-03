705c9ba298690811f9b0ca0cb17735750c2aef7a
Ruben Robles <d8vjork@outlook.com>
2019-02-03 19:25:13 +0000

    W.I.P: Homepage design refresh

--- app/components/home/header.js
+++ app/components/home/header.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
-import {inject as service} from '@ember/service';
+import { inject as service } from '@ember/service';
+// import InViewportMixin from 'ember-in-viewport';
 
 export default Component.extend({
   session: service(),
