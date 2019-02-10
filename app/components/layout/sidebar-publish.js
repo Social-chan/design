d28c9909d99b093598960622f003739f79a24d8b
Ruben Robles <d8vjork@outlook.com>
2019-02-10 18:42:45 +0000

    TL;DR
    - Move sidebars and other layout elements to layout folder
    - Change left sidebar: Anime/manga/music items no longer w/ subsidebar, replaced by search subsidebar
    - Split left subsidebar by layout components

---
+++ app/components/layout/sidebar-publish.js
@@ -0,0 +1,25 @@
+import Component from '@ember/component';
+
+export default class LayoutSidebarPublishComponent extends Component {
+  // createPost: task(function* () {
+  //   const formData = this.getProperties([
+  //     'content',
+  //   ]);
+
+  //   let user = this.get('store').createRecord('post', {
+  //     content: formData.content,
+  //   });
+
+  //   yield user.save().catch((reason) => {
+  //     this.set('errorMessage', reason.error);
+  //   }).then(() => {
+  //     this.transitionToRoute('feed');
+  //   });
+  // }).drop(),
+
+  // actions: {
+  //   create() {
+  //     this.get('createPost').perform();
+  //   }
+  // }
+}
