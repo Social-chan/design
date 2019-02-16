d97a7cfcb7cdfa16e8139d17d726bc89b4976cae
Ruben Robles <d8vjork@outlook.com>
2019-02-16 19:33:49 +0000

    User hovercard redesign

--- app/components/user/hovercard.js
+++ app/components/user/hovercard.js
@@ -1,4 +1,6 @@
 import Component from '@ember/component'
+import { classNames } from '@ember-decorators/component'
 
+@classNames('relative flex flex-col')
 export default class UserHovercard extends Component {
 }
